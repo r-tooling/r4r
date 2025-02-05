9d4bfad0706218e18fa8ce1db7f5c1b0052266e4
Filip Krikava <krikava@gmail.com>
2025-02-05 22:48:07 +0000

    Remove legacy code

--- legacy/frontend/syscalls/exit.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "genericSyscallHeader.hpp"
-namespace frontend::SyscallHandlers {
-HandlerClassDef(SYS_exit_group) : public OnlyEntryLog{};
-HandlerClassDef(SYS_exit) : public OnlyEntryLog{};
-} // namespace frontend::SyscallHandlers
