c7f5b3cc8412cb703548de0915251a6a5d6ce833
Filip Krikava <krikava@gmail.com>
2025-05-27 13:18:06 +0000

    Fix integration test container permissions

--- tests-integration/entrypoint.sh
+++ tests-integration/entrypoint.sh
@@ -1,9 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
+# Ensure R4R_UID and R4R_GID are set (via `-e R4R_UID=$(id -u)` etc)
+: "${R4R_UID:?Need to set R4R_UID}"
+: "${R4R_GID:?Need to set R4R_GID}"
+
+OLD_UID="$(id -u r4r)"
+OLD_GID="$(id -g r4r)"
+
+if [ "$OLD_GID" != "$R4R_GID" ]; then
+	groupmod -g "$R4R_GID" r4r
+fi
+
+if [ "$OLD_UID" != "$R4R_UID" ]; then
+	usermod -u "$R4R_UID" -g "$R4R_GID" r4r
+fi
+
+/usr/bin/find /home/r4r -user "$OLD_UID" -group "$OLD_GID" \
+	-exec chown -h "$R4R_UID":"$R4R_GID" {} +
+
 # Get the GID of the host's Docker group from the Docker socket
-if [ -e /var/run/docker.sock ]; then
+if [[ -e /var/run/docker.sock ]]; then
 	HOST_DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
 	groupmod -g "$HOST_DOCKER_GID" docker
 else
