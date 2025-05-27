3cc6f4b97abd38b8fd916761b3bf03a6391087d4
Filip Krikava <krikava@gmail.com>
2025-05-27 17:14:49 +0000

    Fix the entrypoint

--- tests-integration/entrypoint.sh
+++ tests-integration/entrypoint.sh
@@ -11,31 +11,19 @@ OLD_UID="$(id -u "$HOST_USER")"
 OLD_GID="$(id -g "$HOST_USER")"
 
 if [ "$OLD_GID" != "$HOST_GID" ]; then
-	if getent group "$HOST_GID" >/dev/null; then
-		echo "WARNING: A group with GID $HOST_GID already exists; skipping groupmod for $HOST_USER." >&2
-	else
-		groupmod -g "$HOST_GID" "$HOST_USER"
-	fi
+	(groupmod -g "$HOST_GID" "$HOST_USER" || true)
 fi
 
 if [ "$OLD_UID" != "$HOST_UID" ]; then
-	if getent passwd "$HOST_GID" >/dev/null; then
-		echo "WARNING: A user with UID $HOST_GID already exists; skipping usermod for $HOST_USER." >&2
-	else
-		usermod -u "$HOST_UID" "$HOST_USER"
-	fi
+	(usermod -u "$HOST_UID" "$HOST_USER" || true)
 fi
 
 /usr/bin/find /home/"$HOST_USER" -user "$OLD_UID" -group "$OLD_GID" -exec chown -h "$HOST_UID":"$HOST_GID" {} \;
 
 # Get the GID of the host's Docker group from the Docker socket
 if [[ -e /var/run/docker.sock ]]; then
-	if getent group docker >/dev/null; then
-		HOST_DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
-		groupmod -g "$HOST_DOCKER_GID" docker
-	else
-		echo "WARNING: docker group does not exists" >&2
-	fi
+	HOST_DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
+	(groupmod -g "$HOST_DOCKER_GID" docker || true)
 else
 	echo "WARNING: Docker socket not found at /var/run/docker.sock" >&2
 fi
